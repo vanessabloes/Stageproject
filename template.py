63be17fc817422632caf6573acace2eb690b3833
Thomas <thomas.vanneuville@ugent.be>
2018-06-10 15:30:34 +0200

    werkende pdf output met test variabelen meegegeven

--- template.py
+++ template.py
@@ -1,10 +1,14 @@
 from jinja2 import Template
 from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML
 
 env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template('/home/wall-e/Stageproject/template.html')
+template = env.get_template('template.html')
 
-template_vars = {"title" : "Sales Funnel Report - National",
-                 "national_pivot_table": sales_report.to_html()}
+template_vars = {"test": "vans ook hoor"}
 
 html_out = template.render(template_vars)
+
+html_out = template.render(template_vars)
+
+HTML(string=html_out).write_pdf("outputfile.pdf")
