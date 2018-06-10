3b15675a8776d00378e76f1b3212de045b3d3326
wall-e <vanessa.bloes@student.howest.be>
2018-06-10 21:50:32 +0200

    html report MSI

---
+++ html2pdf.py
@@ -0,0 +1,3 @@
+from weasyprint import HTML
+
+HTML('/home/wall-e/Stageproject/reportdelta.html').write_pdf("test.pdf")
--- template.py
+++ template.py
@@ -1,10 +1,13 @@
 from jinja2 import Template
 from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML
 
 env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template('/home/wall-e/Stageproject/template.html')
+template = env.get_template('template.html')
 
 template_vars = {"title" : "Sales Funnel Report - National",
                  "national_pivot_table": sales_report.to_html()}
 
 html_out = template.render(template_vars)
+
+HTML('/home/wall-e/Documents/Thomas/M17-3261.html').write_pdf("staal1.pdf")
