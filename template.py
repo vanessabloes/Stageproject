690f15925cf431560ad8b1535005a7de4540bcbe
wall-e <vanessa.bloes@student.howest.be>
2018-06-10 15:06:09 +0200

    nieuwere versie

--- template.py
+++ template.py
@@ -1,6 +1,10 @@
 from jinja2 import Template
-with open('template.html') as f:
-    tmpl = Template(f.read())
-print tmpl.render(
-    sample = 'staal1'
-)
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('/home/wall-e/Stageproject/template.html')
+
+template_vars = {"title" : "Sales Funnel Report - National",
+                 "national_pivot_table": sales_report.to_html()}
+
+html_out = template.render(template_vars)
