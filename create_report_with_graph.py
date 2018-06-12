d181ef9393fa421aa0895125eb1fee1034d8667a
wall-e <vanessa.bloes@student.howest.be>
2018-06-12 22:47:16 +0200

    cleanup

--- create_report_with_graph.py
+++ create_report_with_graph.py
@@ -1,28 +1,26 @@
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
-import os
+
 import matplotlib.pyplot as plt
 import numpy as np
 
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 class ReportCreator(object):
 	"""Takes text files, outputs a pdf report"""
-	def __init__(self, textfile1):
+	def __init__(self, textfile1, textfile2):
 		self.textfile1 = textfile1
 		self.textfile2 = textfile2
 
 	def get_values(self):
-		"""txt parsers"""
+		"""txt parser"""
 		with open(self.textfile1) as file:
 			for x, line in enumerate(file):
 				line = line.strip().split('\t')
 				if x == 0:
 					self.sample = line[1][0:8]
 				elif x == 3:
-					self.score = line[1]
-				elif x == 4:
-					self.status = line[1]
-			return self.sample, self.score, self.status
+					self.score = line[1]	
 
 	def create_images(self):
 		"""Create graphs from values"""
@@ -31,11 +29,11 @@ class ReportCreator(object):
 		peaks_base = []
 
 		with open(self.textfile2) as f:
-    		for x, line in enumerate(f):
+    	    for x, line in enumerate(f):
         		line = line.strip().split('\t')
         		if x != 0:
-            	regio.append(line[0]) 
-            	peaks_sample.append(float(line[3]))
+            	    regio.append(line[0]) 
+            	    peaks_sample.append(float(line[3]))
 
 		with open('/home/wall-e/msings/doc/MSI_BASELINE.txt') as b:
     		for x, line in enumerate(b):
@@ -69,12 +67,18 @@ class ReportCreator(object):
 
 if __name__ == '__main__':
 	#Variabelen eventueel meegeven op commandline met argeparse
-	file_name = 'example_input/M18-1605_S10.MSI_Analysis.txt'
+    parser = ArgumentParser(description='description', formatter_class= ArgumentDefaultsHelpFormatter)
+    parser.add_argument('textfile1', type= str)
+	parser.add_argument('textfile2', type= str)
+	parser.add_argument('out', type= str)
+    
+    args = parser.parse_args()
+
 	template = 'template/template.html'
 	style = 'template/style.css'
-	output = 'testpdf.pdf'
 
 	#script
-	report = ReportCreator(file_name)
-	report.get_values()
-	report.create_pdf(template, style, output)
+	report = ReportCreator(args.textfile1, args.textfile1)
+	sample, score = report.get_values()
+    
+	report.create_pdf(template, style, args.out)
