0e62ee79fd6923402d9a6012b35da56dc82eb0c4
Thomas <thomas.vanneuville@ugent.be>
2018-06-13 22:54:42 +0200

    werkt maar plot is nog niet juist

--- create_report.py
+++ create_report.py
@@ -1,12 +1,15 @@
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import matplotlib.pyplot as plt
+import numpy as np
 
 class ReportCreator(object):
 	"""Takes text files, outputs a pdf report"""
-	def __init__(self, textfile1, textfile2):
+	def __init__(self, textfile1, textfile2, peaksfile):
 		self.textfile1 = textfile1
 		self.textfile2 = textfile2
+		self.peaksfile = peaksfile
 
 	def get_values(self):
 		"""txt parsers"""
@@ -20,24 +23,40 @@ class ReportCreator(object):
 					self.score = float(self.score)
 				elif x == 4:
 					self.status = line[1]
-			return self.sample, self.score, self.status
 
-	def get_values2(self):
 		with open(self.textfile2) as file:
 			for x, line in enumerate(file):
 				line = line.strip().split('\t')
 				self.regio = []
 				self.peaks = []
+				self.peaks_base = []
 				if x != 0:
-					regio.append(line[0])
-					peaks.append(int(line[3]))	
+					self.regio.append(line[0])
+					self.peaks.append(float(line[3]))
 
-				
-		
+		with open(self.peaksfile) as file:
+			for x, line in enumerate(file):
+				line = line.strip().split('\t')
+				if x !=0:
+					self.peaks_base.append(float(line[2]))
+
+		return [self.regio,  self.peaks, self.peaks_base], [self.sample, self.score, self.status]
 
 	def create_images(self):
 		"""Create graphs from values"""
-		pass
+		xpos = np.arange(len(self.regio))
+
+		plt.bar(xpos-0.2,self.peaks, width =0.4,label="Patient")
+		plt.bar(xpos+0.2,self.peaks_base, width =0.4,label="Baseline")
+
+		plt.xticks(xpos,self.regio, rotation=10,fontsize=4)
+		plt.ylabel("Aantal pieken")
+		plt.xlabel("Loci")
+		plt.title('Pieken per loci: Patient vs Baseline', fontsize = 20)
+		plt.legend()
+		fig1 = plt.gcf()
+		plt.draw()
+		fig1.savefig('tessstttyyy.png', dpi=100)
 	
 	def create_pdf(self, html_template, stylesheet, output):
 		"""Combines html, jinja variables and css into pdf"""
@@ -61,14 +80,14 @@ if __name__ == '__main__':
 
 	#Variabelen eventueel meegeven op commandline met argeparse
 	template = 'template/template.html'
-	style = 'template/style.css'
+	style = 'template/img/style.css'
 	
  
 	#script
-
-	report = ReportCreator(args.file1, args.file2)
-	sample, score, status = report.get_values()
-	print score
-	print type(score)
-	report.get_values2()
-	report.create_pdf(template, style, args.out)
+	peaksfile = 'example_input/MSI_BASELINE.txt'
+	report = ReportCreator(args.file1, args.file2, peaksfile)
+	list2, list1 = report.get_values()
+	print list2
+	print list1
+	report.create_images()
+	#report.create_pdf(template, style, args.out)
